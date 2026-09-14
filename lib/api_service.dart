268a9539d7d4a432de446033d932aee8a432e6bc
Hello-CMS-AI <hellocms.ai@gmail.com>
2026-09-14 15:18:07 +0530

    feat: sort bills from oldest to newest (first bill of the day to current)

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -258,7 +258,7 @@ class ApiService {
           'limit=100&'
           'depth=1&'
           'page=$page&'
-          'sort=-createdAt';
+          'sort=createdAt';
 
       final res = await http.get(
         Uri.parse(url),
