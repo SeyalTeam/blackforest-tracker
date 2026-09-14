0487d25bfecedfb71df7e86e55de56b85cc35d5f
Hello-CMS-AI <hellocms.ai@gmail.com>
2026-09-14 17:15:54 +0530

    fix: import intl package in api_service.dart

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
